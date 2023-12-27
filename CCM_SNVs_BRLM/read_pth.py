32c8758da78447a2ec84745427bde48bdf019219
wangyiqi806643897 <806643897@qq.com>
2023-12-27 10:34:20 +0800

    Update read_pth.py

--- CCM_SNVs_BRLM/read_pth.py
+++ CCM_SNVs_BRLM/read_pth.py
@@ -1,10 +1,10 @@
 import torch
 
 pthfile = r'checkpoints/model-45.pth' #faster_rcnn_ckpt.pth
-net = torch.load(pthfile,map_location=torch.device('cpu')) # 由于模型原本是用GPU保存的，但我这台电脑上没有GPU，需要转化到CPU上
+net = torch.load(pthfile,map_location=torch.device('cpu')) # Running on CPU
 
-print(type(net)) # 类型是 dict
-print(len(net)) # 长度为 4，即存在四个 key-value 键值对
+print(type(net)) # dict type
+print(len(net)) #  4 key-value
 
 for k in net.keys():
- print(k) # 查看四个键，分别是 model,optimizer,scheduler,iteration
+ print(k) # 4 keys: model,optimizer,scheduler,iteration
